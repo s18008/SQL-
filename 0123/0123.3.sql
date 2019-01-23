ac39f6fd4efc29a621383767afac8c3d4f5d14fd
s18008 <s18008@std.it-college.ac.jp>
2019-01-23 15:28:23 +0900

    1月23日の課題

---
+++ 0123/0123.3.sql
@@ -0,0 +1,2 @@
+insert into my_employees(id,last_name,first_name,userid,salary) 
+values (2,'Dancs','Betty','bdancs',860);
---
+++ 0123/0123.4.sql
@@ -0,0 +1,2 @@
+insert into my_employees(id,last_name,first_name,userid,salary) 
+values (&ID,'&LAST_NAME','&FIRST_NAME','&USERID',&SALARY);
